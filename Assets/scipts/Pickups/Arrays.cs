9fb8cf29c77511eaa1ee6fee8970a457afc428d0
EthanC26 <ethan.cullen@torontofilmschool.ca>
2025-02-22 03:16:34 -0700

    lab 5 Incomplete

---
+++ Assets/scipts/Pickups/Arrays.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    private IPickup[] ranDrop;
+    private IPickup selectedPower;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ranDrop = new IPickup[]
+        {
+            new lives(),
+            new score(),
+            new Powerup()
+        };
+        
+       IPickup selectedPower = ranDrop[Random.Range(0, ranDrop.Length)];
+
+        Debug.Log("Selected PowerUp: " + selectedPower.GetType().Name);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("player"))
+        {
+            IPickup pickup = GetComponent<IPickup>();
+
+        }
+        Destroy(gameObject);
+    }
+}
