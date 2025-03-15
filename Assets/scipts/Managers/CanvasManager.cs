4f1f0640ca68ef8e86b83aed6112558c5001fda2
EthanC26 <ethan.cullen@torontofilmschool.ca>
2025-03-15 00:17:19 -0400

    lab 8 Done

--- Assets/scipts/Managers/CanvasManager.cs
+++ Assets/scipts/Managers/CanvasManager.cs
@@ -20,6 +20,7 @@ public class CanvasManager : MonoBehaviour
     public GameObject mainMenu;
     public GameObject settingsMenu;
     public GameObject pauseMenu;
+    
 
     [Header("Text")]
     public TMP_Text volSliderText;
@@ -27,6 +28,12 @@ public class CanvasManager : MonoBehaviour
 
     [Header("Slider")]
     public Slider volSlider;
+
+    private void OnResume()//this is so the resume button will work for the pause menu
+    {
+        setMenus(null, pauseMenu);
+        Time.timeScale = 1.0f;
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,7 +44,7 @@ public class CanvasManager : MonoBehaviour
         if (settingsBtn) settingsBtn.onClick.AddListener(() => setMenus(settingsMenu, mainMenu));
         if (backBtn) backBtn.onClick.AddListener(() => setMenus(mainMenu, settingsMenu));
         if(quitBtn) quitBtn.onClick.AddListener(QuitGame);
-        if (resumeBtn) resumeBtn.onClick.AddListener(() => setMenus(null, pauseMenu));
+        if (resumeBtn) resumeBtn.onClick.AddListener(OnResume);
         if (mainMenuBtn) mainMenuBtn.onClick.AddListener(() => SceneManager.LoadScene("TitleScreen"));
 
         if (volSlider)
@@ -102,10 +109,14 @@ public class CanvasManager : MonoBehaviour
             if(pauseMenu.activeSelf)
             {
                 //do something - hint for lab
+                Time.timeScale = 0.0f;
             }
+          
             else
             {
                 //do something else
+                //only works with pressing P
+                Time.timeScale = 1.0f;
             }
         }
     }
